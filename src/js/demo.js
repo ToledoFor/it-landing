15becd567d1e119fe332e6ae291b8425a513db51
Dmitriy Olhovsky <dmitriy.olhovsky@beetroot.se>
2022-12-08 10:43:53 +0200

    Hero section completed

--- src/js/demo.js
+++ src/js/demo.js
@@ -2,3 +2,33 @@ const demo = () => 'Webpack Boilerplate v5.14.0 - SASS/PostCSS, ES6/7, browser s
 
 // eslint-disable-next-line no-console
 console.log(demo());
+
+// //console.log(v1); // error
+
+// let v1;
+// const v2 = 'string'; // "string" `string` "10"
+// // 0 - false
+// // "0" - true
+// const num1 = 10;
+
+// console.log(v3); // underfined
+
+// const obj1 = {
+//     name: 'test',
+//     value: 123,
+// }
+
+// obj1.name = 'test1';
+
+// var v3;
+
+// if (v3 == 10) {
+
+// } else {
+
+// }
+
+// function myFunction(test) {
+
+//     return test;
+// }
